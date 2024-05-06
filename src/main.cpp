1ee1daf08e4a3a20c01e44dc7dd469714f409e4e
John Link <jwl9vq@virginia.edu>
2024-05-05 23:06:08 -0400

    added charger comms code

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,17 @@
 
 uint16_t const max_voltage = 3984; // 3984 = 398.4V
 uint16_t const max_current = 50; // 50 = 5.0A
-uint8_t enable_data[] = {(max_voltage >> 8) & 0xFF, max_voltage & 0xFF,(max_current >> 8) & 0xFF,max_current & 0xFF,0x1};
-uint8_t disable_data[] = {(max_voltage >> 8) & 0xFF, max_voltage & 0xFF,(max_current >> 8) & 0xFF,max_current & 0xFF,0x0};
-CanMsg enable_msg = CanMsg(CanStandardId(0x1806E5F4), 5, enable_data);
-CanMsg disable_msg = CanMsg(CanStandardId(0x1806E5F4), 5, disable_data);
+uint8_t enable_data[] = {(max_voltage >> 8) & 0xFF, max_voltage & 0xFF, (max_current >> 8) & 0xFF, max_current & 0xFF,0x0};
+uint8_t disable_data[] = {(max_voltage >> 8) & 0xFF, max_voltage & 0xFF, (max_current >> 8) & 0xFF, max_current & 0xFF,0x1};
+CanMsg enable_msg = CanMsg(CanExtendedId(0x1806E5F4), 5, enable_data);
+CanMsg disable_msg = CanMsg(CanExtendedId(0x1806E5F4), 5, disable_data);
 uint16_t count = 0;
 
 void setup() {
   
   Serial.begin(9600);
   while (!Serial) { }
-  if (!CAN.begin(CanBitRate::BR_250k))
+  if (!CAN.begin(CanBitRate::BR_500k))
   {
     Serial.println("CAN.begin(...) failed.");
     for (;;) {}
@@ -28,6 +28,7 @@ void loop() {
 
   if(count > 50){
     int ret = CAN.write(enable_msg);
+    // Serial.println(enable_msg);
     if(!(ret == 0 || ret == 1)){
         Serial.print("CAN Error: ");
         Serial.println(ret);
@@ -48,7 +49,11 @@ void loop() {
     Serial.println("A");
     uint8_t status = msg.data[4];
     Serial.print("Status: ");
-    Serial.println(status & 0x0);
+    Serial.println(status & 0x1); //Hardware Fault
+    Serial.println(status >> 0x1 & 0x1); // Charger OverTemp
+    Serial.println(status >> 0x2 & 0x1); // Input voltage fault
+    Serial.println(status >> 0x3 & 0x1); // Battery connection fault
+    Serial.println(status >> 0x4 & 0x1); // communication fault
 
   }
 
